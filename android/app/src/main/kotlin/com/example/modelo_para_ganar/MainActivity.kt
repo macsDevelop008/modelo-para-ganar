51df915b5879ea77e650f38e795a13cb5950d9a5
macsDevelop008 <47404938+macsDevelop008@users.noreply.github.com>
2024-10-12 07:21:19 -0500

    🎉  Se crea el proyecto base con la estructura para la arquitectura limpia y se importan las librerías para el manejo de variables de entorno y rutas.

---
+++ android/app/src/main/kotlin/com/example/modelo_para_ganar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.modelo_para_ganar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  // Incicializar varriables entorno
+  await dotenv.load(fileName: ".env");
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
